4fec6ac69108b77082ff662c480bf2f40ecb80ed
Matt Farmer <mrfarmer777@gmail.com>
2018-06-17 03:31:13 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,23 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    max=Show.maximum("rating")
+    Show.maximum("rating")
+  end
+
+  def self.most_popular_show
+    max=Show.maximum('rating')
     Show.where("rating=?",max).first
   end
+
+  def self.lowest_rating
+    Show.minimum("rating")
+  end
+
+  def least_popular_show
+    min=Show.maximum('rating')
+    Show.where("rating=?",min).first
+  end
+  
 
 
 end
