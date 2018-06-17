bcf846efafffafddc14ce867ee320fe5cc6b6059
Matt Farmer <mrfarmer777@gmail.com>
2018-06-17 03:32:30 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
     Show.minimum("rating")
   end
 
-  def least_popular_show
+  def self.least_popular_show
     Show.where("rating=?",Show.lowest_rating).first
   end
 
